a3eb4a0bc4189cbf13b4bd9214772c624255d640
SRAKIB17 <rakibulssc5@gmail.com>
2023-06-04 21:16:43 +0600

    'doen

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -4,7 +4,6 @@ import MyIntro from "@/src/components/Home/MyIntro"
 import MyWorkProcess from "@/src/components/Home/MyWorkProcess"
 import Pricing from "@/src/components/Home/Pricing"
 import Testimonial from "@/src/components/Home/Testimonial"
-import { getDictionary } from "@/src/language/language"
 import { createContext } from "react"
 export default async function Home() {
   // console.log(await getDictionary('bn'))
